ee797f57c768948a4cfdbfe68eb9eb12e7bb9254
n6ux <kelechalt@gmail.com>
2024-07-24 13:03:01 +0100

    changes

--- main.lua
+++ main.lua
@@ -1,7 +1,43 @@
-local module = {}
+local coregui = game:GetService("CoreGui")
 
-function module:yeah()
-    print("okay")
+local lib = {}
+lib.__index = lib
+
+lib.initialized = false
+lib.activescreengui = nil
+
+function lib:Initialize()
+    local screengui = Instance.new("ScreenGui")
+    screengui.Parent = coregui
+    screengui.Name = "NXUI"
+
+    lib.initialized = true
+    lib.activescreengui = screengui
 end
 
-return module
+function lib:CreateWindow()
+    if lib.initialized == true then
+        local newwindow = {}
+        setmetatable(newwindow, lib)
+
+        newwindow.Dragabble = false
+        newwindow.Size = UDim2.new()
+        newwindow.Position = UDim2.new()
+        newwindow.Name = ""
+
+        function newwindow.Render()
+            local frame = Instance.new("Frame")
+            frame.Parent = lib.activescreengui
+
+            frame.Name = newwindow.Name.."_WINDOW"
+            frame.Size = newwindow.Size
+            frame.Position = newwindow.Position
+        end
+
+        return newwindow
+    else
+        warn("[NXUI]: NXUI has not been initialized, event failed.")
+    end
+end
+
+return lib
