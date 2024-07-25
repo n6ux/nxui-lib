0c67501ea9464aeef43082b99719132b7317c424
n6ux <kelechalt@gmail.com>
2024-07-25 17:21:13 +0100

    a

--- main.lua
+++ main.lua
@@ -39,7 +39,7 @@ function lib:CreateAndRenderWindow(properties: {}) --draggable, size, pos, name
         textlabel.Size = UDim2.new(frame.Size.X.Scale, frame.Size.X.Offset, 0, 35)
         textlabel.Position = UDim2.new(0, 25, 0, 0)
         textlabel.Text = properties.name
-        textlabel.BackgroundTransparency = .65
+        textlabel.BackgroundTransparency = .35
         textlabel.TextColor3 = Color3.fromRGB(0, 0, 0)
         textlabel.TextScaled = true
         textlabel.TextXAlignment = Enum.TextXAlignment.Left
