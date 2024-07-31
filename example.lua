9ab17a24f0f860e8e3fb65a252e9bd45ee76f252
n6ux <n6uxxwrd@gmail.com>
2024-07-31 18:39:18 +0100

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,9 +1,1 @@
-local lib = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/n6ux/nxui-lib/main/main.lua"))()
-
-lib:Initialize()
-
-lib:CreateAndRenderWindow({
-    draggable = true,
-    pos = UDim2.new(0.5, 0, 0.5, 0),
-    name = "testing"
-})
+--kinda still working on it, ill make this later
